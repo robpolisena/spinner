6e2863139ce6cf85b25746100e0e428fe3a0fb84
Rob <robpolisena@gmail.com>
2020-06-30 18:27:01 +0000

    Spinner I

--- spinner1.js
+++ spinner1.js
@@ -14,7 +14,7 @@ setTimeout(() => {
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
+  process.stdout.write('\r\\   ');
 }, 700);
 
 setTimeout(() => {
@@ -23,7 +23,7 @@ setTimeout(() => {
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r/   '); 
+  process.stdout.write('\r/   ');
 }, 1100);
 setTimeout(() => {
   process.stdout.write('\r-   ');
@@ -31,7 +31,7 @@ setTimeout(() => {
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
+  process.stdout.write('\r\\   ');
 }, 1500);
 
 setTimeout(() => {
