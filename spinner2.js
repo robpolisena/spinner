8a53429f1ec802f31db37b78d3290a8492c44e34
Rob <robpolisena@gmail.com>
2020-06-30 18:25:51 +0000

    Spinner II

--- spinner2.js
+++ spinner2.js
@@ -1,6 +1,6 @@
-let motion = '|/-\\|'
+let motion = '|/-\\|';
 let x  = 0;
 for (let char of motion) {
   setTimeout(() =>  process.stdout.write(`\r${(char)}   `), x);
-    x += 200
-  };
+  x += 200;
+}
